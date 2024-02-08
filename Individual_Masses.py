f6f94b71027e31b30fbeaaab2655c3f2f48e81f8
lohit1632 <133949224+lohit1632@users.noreply.github.com>
2024-02-08 20:00:23 +0530

    Create Individual_Masses.py

---
+++ Individual_Masses.py
@@ -0,0 +1,6 @@
+import math
+mass1 = (chirp_mass*(1+1)**(0.2))/((1)**(0.6))
+mass2 = mass1*(1)
+# total_mass = 10.0  # Replace with your actual total mass
+# symmetric_mass_ratio = 0.2  # Replace with your actual symmetric mass ratio
+print(mass1/solar_mass,mass2/solar_mass)
